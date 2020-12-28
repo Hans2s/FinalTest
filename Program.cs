63343daf09b7288a3646c3d92f873fac75ae57a1
Hans2s <hansamui@outlook.com>
2020-12-28 13:07:36 +0300

    Add project files.

---
+++ Program.cs
@@ -0,0 +1,16 @@
+﻿using System;
+
+namespace FinalTest
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            //writing the variables 
+            double a = 1.234;
+            double b = 0.756;
+            double c = 2.23;
+            double d = -1.653;
+        }
+    }
+}
